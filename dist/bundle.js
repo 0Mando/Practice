5972b78095ff26f3a9878ea2cdd27f2df1a5a590
0HungryDeveloper <hungrystudentdev@gmail.com>
2022-08-05 02:57:18 -0500

    Deployment order

--- dist/bundle.js
+++ dist/bundle.js
@@ -15,6 +15,7 @@ var next_btn = document.getElementById('next');
 fetch("".concat(API_URL))
     .then(function (response) { return response.json(); })
     .then(function (data) {
+    filterData(data);
 });
 /**
  * Filter the content depending on whether or not there is a value in the property.
@@ -22,10 +23,10 @@ fetch("".concat(API_URL))
  */
 function filterData(countries) {
     var body = '';
-    for (var i = initialIndex; i < elementsPerPage; i++) {
+    for (var i = initialIndex; i < countries.length; i++) {
         // Properties to display
         body +=
-            "<tr>\n            <td>".concat(countries[i].name.official, "</td>\n            <td>").concat(getCapital(countries[i].capital), "</td>\n            <td>").concat(countries[i].region, "</td>\n            <td>").concat(countries[i].languages, "</td>\n            <td>").concat(countries[i].population, "</td>\n            <td>").concat(countries[i].flag, "</td>\n        </tr>\n        ");
+            "<tr>\n            <td>".concat(countries[i].name.official, "</td>\n            <td>").concat(getCapital(countries[i].capital), "</td>\n            <td>").concat(countries[i].region, "</td>\n            <td>").concat(getLanguage(countries[i].languages), "</td>\n            <td>").concat(countries[i].population, "</td>\n            <td>").concat(countries[i].flag, "</td>\n        </tr>");
     }
     document.getElementById('data').innerHTML = body;
 }
@@ -45,49 +46,18 @@ function getCapital(capital) {
 function getLanguage(languages) {
     return languages ? Object.values(languages) : "No language";
 }
-// function getData(data:any){
-//     let j:number = currentIndex;
-//     for(let i = 0; i < data.length; i++){
-//         if(data[i].capital === undefined){
-//             return "No data";
-//         }
-//     }
-//     let asc:any = data.sort((a:any,b:any)=>{
-//         if(a.name.official > b.name.official){
-//             return 1
-//         }
-//         if(a.name.official < b.name.official){
-//             return -1
-//         }
-//         return 0
-//     })
-//     next_btn.addEventListener('click', (e)=>{
-//         console.log("pressed");
-//         currentIndex = currentIndex + 25;
-//         nextIndex = nextIndex + 25;
-//     })
-//     let body:string = '';
-//     for(j = currentIndex; j < nextIndex; j++){
-//         body +=
-//         `<tr>
-//             <td>${data[j].name.official}</td>
-//             <td>${data[j].capital}</td>
-//             <td>${data[j].region}</td>
-//             <td>${data[j].languages}</td>
-//             <td>${data[j].population}</td>
-//             <td>${data[j].flag}</td>
-//         </tr>
-//         `
-//     }
-//     document.getElementById('data')!.innerHTML = body;
-// }
-// next_btn.addEventListener('click', (e)=>{
-//     console.log("pressed");
-//     currentIndex = currentIndex + 25;
-//     nextIndex = nextIndex + 25;
-//     console.log(currentIndex);
-//     console.log(nextIndex);
-// })
+var toggle = document.getElementById('toggle');
+toggle.addEventListener('click', function (e) {
+    // console.log('Togle');
+    toggle.textContent = 'Descending mode';
+    // if(toggle.value === 'Ascending mode'){
+    //     toggle.value = 'Descending mode';
+    //     console.log(toggle.value);
+    // }else{
+    //     toggle.value = 'Ascending mode';
+    //     console.log(toggle.value)
+    // }
+});
 
 /******/ })()
 ;
--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,67 @@ let elementsPerPage: number = 25;
 
 
 //* Elementos del documento
-const previous_btn = document.getElementById('previous')!;
-const next_btn = document.getElementById('next')!;
+const previous_btn = document.getElementById('previous') as HTMLButtonElement;
+const next_btn = document.getElementById('next') as HTMLButtonElement;
+const toggle = document.getElementById('toggle') as HTMLButtonElement;
 
 
 
 fetch(`${API_URL}`)
-.then(response => response.json())
-.then(data => {
-    filterData(data)
-})
+    .then(response => response.json())
+    .then(data => {
 
+        data.sort((a:any, b:any)=>{
+            if(a.name.official > b.name.official){
+                return 1;
+            }
+            if(a.name.official < b.name.official){
+                return -1;
+            }
+            return 0;
+        });
+        createTable(data);
+
+        toggle.addEventListener('click',(e)=>{
+            if(toggle.value === 'Ascending Mode'){
+                toggle.textContent = 'Descending Mode';
+                toggle.value = 'Descending Mode';
+                data.sort((a:any, b:any)=>{
+                    if(a.name.official > b.name.official){
+                        return 1;
+                    }
+                    if(a.name.official < b.name.official){
+                        return -1;
+                    }
+                    return 0;
+                });
+
+                createTable(data);
+
+            }else if(toggle.value === 'Descending Mode'){
+                toggle.textContent = 'Ascending Mode';
+                toggle.value = 'Ascending Mode';
+
+                data.sort((a:any, b:any)=>{
+                    if(b.name.official > a.name.official){
+                        return 1;
+                    }
+                    if(b.name.official < a.name.official){
+                        return -1;
+                    }
+                    return 0;
+                });
+
+                createTable(data);
+            }
+        });
+    });
 
 /**
- * Filter the content depending on whether or not there is a value in the property.
- * @param countries Country object
+ * Create the code to make the table in the document.
+ * @param countries Information of countries.
  */
-function filterData(countries:any){
-
+function createTable(countries:any):void{
     let body:string = '';
 
     for(let i = initialIndex; i < countries.length; i++){
@@ -36,18 +79,19 @@ function filterData(countries:any){
             <td>${getLanguage(countries[i].languages)}</td>
             <td>${countries[i].population}</td>
             <td>${countries[i].flag}</td>
-        </tr>
-        `
+        </tr>`
     }
+
     document.getElementById('data')!.innerHTML = body;
 }
+
 
 /**
  * Get the capital of the country.
  * @param capital Capital property.
  * @returns Capital information.
  */
-function getCapital(capital:string){
+function getCapital(capital:string):string{
     return capital ? capital : "No capital";
 }
 
@@ -56,6 +100,7 @@ function getCapital(capital:string){
  * @param languages Language property.
  * @returns Languages information.
  */
-function getLanguage(languages:any){
+function getLanguage(languages:any): unknown[] | string{
     return languages ? Object.values(languages) : "No language";
 }
+
